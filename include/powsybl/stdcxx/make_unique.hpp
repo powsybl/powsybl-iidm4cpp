6af6041f9f406a4d5df786b58fd907f7a0428d4f
Mathieu BAGUE <mathieu.bague@rte-france.com>
2017-12-23 12:07:51 +0100

    Fix warnings / compilations errors with gcc

--- include/powsybl/stdcxx/make_unique.hpp
+++ include/powsybl/stdcxx/make_unique.hpp
@@ -25,7 +25,7 @@ namespace stdcxx {
 template <typename T, typename... Args>
 std::unique_ptr<T> make_unique(Args&&... args) {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-};
+}
 
 }
 
