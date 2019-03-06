125b3aa3ecac192c23796e0b60cb51b4fde1a4ea
Mathieu BAGUE <mathieu.bague@rte-france.com>
2019-03-06 14:24:23 +0100

    Fix compilation error with clang    error: template parameter redefines default argument

--- include/powsybl/logging/MessageFormat.hxx
+++ include/powsybl/logging/MessageFormat.hxx
@@ -83,7 +83,7 @@ std::string toString(const std::vector<T>& vect) {
     return oss.str();
 }
 
-template <std::size_t N, typename T, typename = typename std::enable_if<std::is_enum<T>::value>::type>
+template <std::size_t N, typename T, typename>
 std::string toString(const std::array<std::string, N>& names, T value) {
     unsigned int val = static_cast<unsigned int>(value);
     return (val < N) ? names.at(val) : format("%1%", val);
