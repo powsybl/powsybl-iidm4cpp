5eab5244038ce4c3625c1ab33c1d6698396b6ca6
Sébastien LAIGRE <slaigre@silicom.fr>
2020-04-03 12:14:36 +0200

    Fix demangle function on windows (removes prefix class/union/struct/enum)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- src/stdcxx/demangle.cpp
+++ src/stdcxx/demangle.cpp
@@ -15,7 +15,24 @@
 namespace stdcxx {
 
 std::string demangle(const char* name) {
+    //return boost::core::demangle(name);
+#if defined __GNUC__
     return boost::core::demangle(name);
+#else 
+    // under windows typeid(T).name() does not return a mangled name, but returns :
+    //  - "class T" if T is a class
+    //  - "struct T" if T is a struct
+    //  - "enum T" if T is an enum
+    //  - "union T" if T is an union
+    // so remove this useless prefix by removing everything found before the last ' ' character
+    std::string tmpString = name;
+    std::size_t spaceIndex = tmpString.rfind(" ");
+    if (spaceIndex != std::string::npos) {
+        std::size_t pos = spaceIndex + 1;
+        tmpString = tmpString.substr(pos, tmpString.size() - pos);
+    }
+    return boost::core::demangle(tmpString.c_str());
+#endif
 }
 
 template <>
