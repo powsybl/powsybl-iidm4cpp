b7366e2ee1f2e94d6eb621ad32ac0922d08fdc80
Sébastien LAIGRE <slaigre@silicom.fr>
2020-04-03 12:14:02 +0200

    Fix test: on windows, areSame on a temporary objects returns false
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- test/logging/LoggerFactoryTest.cpp
+++ test/logging/LoggerFactoryTest.cpp
@@ -23,18 +23,18 @@ BOOST_AUTO_TEST_CASE(test) {
     Logger& unknown1 = LoggerFactory::getLogger("unknown1");
     Logger& unknown2 = LoggerFactory::getLogger("unknown2");
     Logger& unknown3 = LoggerFactory::getLogger<LoggerFactory>();
-    BOOST_TEST(stdcxx::areSame(typeid(NoopLogger), typeid(unknown1)));
-    BOOST_TEST(stdcxx::areSame(typeid(NoopLogger), typeid(unknown2)));
-    BOOST_TEST(stdcxx::areSame(typeid(NoopLogger), typeid(unknown3)));
-    BOOST_TEST(stdcxx::areSame(unknown1, unknown2));
-    BOOST_TEST(stdcxx::areSame(unknown1, unknown3));
+    BOOST_CHECK(typeid(NoopLogger) == typeid(unknown1));
+    BOOST_CHECK(typeid(NoopLogger) == typeid(unknown2));
+    BOOST_CHECK(typeid(NoopLogger) == typeid(unknown3));
+    BOOST_CHECK(typeid(unknown1) == typeid(unknown2));
+    BOOST_CHECK(typeid(unknown1) == typeid(unknown3));
 
     LoggerFactory::getInstance().addLogger("console", stdcxx::make_unique<ConsoleLogger>());
     Logger& consoleLogger = LoggerFactory::getLogger("console");
     BOOST_TEST(stdcxx::areSame(typeid(ConsoleLogger), typeid(consoleLogger)));
     LoggerFactory::getInstance().removeLogger("console");
     Logger& consoleLogger2 = LoggerFactory::getLogger("console");
-    BOOST_TEST(stdcxx::areSame(typeid(NoopLogger), typeid(consoleLogger2)));
+    BOOST_CHECK(typeid(NoopLogger) == typeid(consoleLogger2));
 
     LoggerFactory::getInstance().addLogger<LoggerFactory>(stdcxx::make_unique<NoopLogger>());
     Logger& noopLogger = LoggerFactory::getLogger<LoggerFactory>();
