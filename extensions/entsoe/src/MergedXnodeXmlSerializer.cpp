d23a9ebf3dd12a5c39b0e81119eda675f9036046
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-04-29 13:17:39 +0200

    Extension versioning (#85)
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- extensions/entsoe/src/MergedXnodeXmlSerializer.cpp
+++ extensions/entsoe/src/MergedXnodeXmlSerializer.cpp
@@ -26,7 +26,7 @@ namespace extensions {
 namespace entsoe {
 
 MergedXnodeXmlSerializer::MergedXnodeXmlSerializer() :
-    ExtensionXmlSerializer("mergedXnode", "network", "http://www.itesla_project.eu/schema/iidm/ext/merged_xnode/1_0", "mxn") {
+    AbstractExtensionXmlSerializer("mergedXnode", "network", "mxn", "http://www.itesla_project.eu/schema/iidm/ext/merged_xnode/1_0") {
 }
 
 std::unique_ptr<Extension> MergedXnodeXmlSerializer::read(Extendable& extendable, converter::xml::NetworkXmlReaderContext& context) const {
