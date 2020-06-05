41e286b54ac28dd30c0ad45746adf5d4ce04ed3b
Sébastien Laigre <slaigre@silicom.fr>
2020-06-05 17:27:20 +0200

    Add ActivePowerControlAdder (#107)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

---
+++ extensions/iidm/include/powsybl/iidm/extensions/iidm/ActivePowerControlAdder.hpp
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef POWSYBL_IIDM_EXTENSIONS_IIDM_ACTIVEPOWERCONTROLADDER_HPP
+#define POWSYBL_IIDM_EXTENSIONS_IIDM_ACTIVEPOWERCONTROLADDER_HPP
+
+#include <powsybl/iidm/ExtensionAdder.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace extensions {
+
+namespace iidm {
+
+class ActivePowerControlAdder : public ExtensionAdder {
+public:
+    /**
+     * Constructor
+     */
+    explicit ActivePowerControlAdder(Extendable& extendable);
+
+    /**
+     * Destructor
+     */
+    ~ActivePowerControlAdder() noexcept override = default;
+
+    /**
+     * Copy constructor
+     */
+    ActivePowerControlAdder(const ActivePowerControlAdder&) = default;
+
+    /**
+     * Move constructor
+     */
+    ActivePowerControlAdder(ActivePowerControlAdder&&) = default;
+
+    /**
+     * Copy assignment operator
+     */
+    ActivePowerControlAdder& operator=(const ActivePowerControlAdder&) = delete;
+
+    /**
+     * Move assignment operator
+     */
+    ActivePowerControlAdder& operator=(ActivePowerControlAdder&&) = delete;
+
+    /**
+     * Set droop value
+     *
+     * @param droop The droop value
+     *
+     * @return this ActivePowerControlAdder object
+     */
+    ActivePowerControlAdder& withDroop(double droop);
+
+    /**
+     * Set participate
+     *
+     * @param participate The participate status
+     *
+     * @return this ActivePowerControlAdder object
+     */
+    ActivePowerControlAdder& withParticipate(bool participate);
+
+protected:
+    /**
+     * Creates the ActivePowerControl extension.
+     *
+     * @param extendable the extendable
+     *
+     * @return the extension
+     */
+    std::unique_ptr<Extension> createExtension(Extendable& extendable) override;
+
+private:
+    bool m_participate = false;
+
+    double m_droop = 0.0;
+};
+
+}  // namespace iidm
+
+}  // namespace extensions
+
+}  // namespace iidm
+
+}  // namespace powsybl
+
+#endif  // POWSYBL_IIDM_EXTENSIONS_IIDM_ACTIVEPOWERCONTROLADDER_HPP
