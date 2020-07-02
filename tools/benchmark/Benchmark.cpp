10c67cdd0120d46ca781e158b808f78b934bc181
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-07-02 17:25:29 +0200

    Improve the benchmark tool to support extensions
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- tools/benchmark/Benchmark.cpp
+++ tools/benchmark/Benchmark.cpp
@@ -5,27 +5,77 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+#include <regex>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/dll/runtime_symbol_info.hpp>
+#include <boost/dll/shared_library.hpp>
 #include <boost/program_options.hpp>
 
+#include <libxml/parser.h>
+
+#include <powsybl/iidm/ExtensionProviders.hpp>
 #include <powsybl/iidm/Network.hpp>
+#include <powsybl/iidm/converter/xml/ExtensionXmlSerializer.hpp>
 #include <powsybl/logging/ConsoleLogger.hpp>
 #include <powsybl/logging/LoggerFactory.hpp>
 #include <powsybl/stdcxx/format.hpp>
 #include <powsybl/stdcxx/make_unique.hpp>
 
+/**
+ * Helper class to load/unload properly LibXml2
+ */
+class LibXml2 {
+public:
+    LibXml2() {
+        xmlInitParser();
+    }
+
+    ~LibXml2() {
+        xmlCleanupParser();
+    }
+};
+
+void loadExtensions(const std::string& strPaths) {
+    std::regex fileRegex(stdcxx::format(".*\\%1%.*", boost::dll::shared_library::suffix().string()));
+
+    // Load extensions from libraries found in the same directory that this binary
+    powsybl::iidm::ExtensionProviders<powsybl::iidm::converter::xml::ExtensionXmlSerializer>::getInstance().loadExtensions(boost::dll::program_location().parent_path().string(), fileRegex);
+
+    if (!strPaths.empty()) {
+        std::vector<std::string> paths;
+        boost::algorithm::split(paths, strPaths, [](char c) { return c == boost::filesystem::path::separator; });
+        for (const auto& path : paths) {
+            powsybl::iidm::ExtensionProviders<powsybl::iidm::converter::xml::ExtensionXmlSerializer>::getInstance().loadExtensions(path, fileRegex);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     const char* const INPUT_FILE = "input-file";
     const char* const OUTPUT_FILE = "output-file";
+    const char* const EXT_PATH = "ext-path";
 
     boost::program_options::options_description desc("Options");
     desc.add_options()
         (INPUT_FILE, boost::program_options::value<std::string>()->required())
-        (OUTPUT_FILE, boost::program_options::value<std::string>()->required());
+        (OUTPUT_FILE, boost::program_options::value<std::string>()->required())
+        (EXT_PATH, boost::program_options::value<std::string>()->default_value(""));
 
     try {
+        // Use the RAII to load/unload LibXml2
+        LibXml2 libxml2;
+
         boost::program_options::variables_map vm;
         boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
         boost::program_options::notify(vm);
+
+        // Setup the logger
+        auto logger = stdcxx::make_unique<powsybl::logging::ConsoleLogger>(powsybl::logging::Level::DEBUG);
+        powsybl::logging::LoggerFactory::getInstance().addLogger("powsybl::iidm::converter::xml::NetworkXml", std::move(logger));
+
+        // Load the XIIDM extensions
+        loadExtensions(vm[EXT_PATH].as<std::string>());
 
         const auto& inputFile = vm[INPUT_FILE].as<std::string>();
         std::ifstream inputStream(inputFile);
@@ -40,9 +90,6 @@ int main(int argc, char** argv) {
             std::cerr << stdcxx::format("Unable to open file '%1%' for writing", outputFile) << std::endl;
             return EXIT_FAILURE;
         }
-
-        auto logger = stdcxx::make_unique<powsybl::logging::ConsoleLogger>();
-        powsybl::logging::LoggerFactory::getInstance().addLogger("powsybl::iidm::converter::xml::NetworkXml", std::move(logger));
 
         const powsybl::iidm::Network& network = powsybl::iidm::Network::readXml(inputStream);
         powsybl::iidm::Network::writeXml(outputStream, network);
