0e657ecd56caf1a1523d9f9e668ec76cefd74564
Sébastien Laigre <slaigre@silicom.fr>
2020-07-30 10:55:08 +0200

    [IIDM v1.2] Use new extension API for TwoWindingsTransformerPhaseAngleClock (#120) (#151)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- extensions/iidm/test/TwoWindingsTransformerPhaseAngleClockTest.cpp
+++ extensions/iidm/test/TwoWindingsTransformerPhaseAngleClockTest.cpp
@@ -10,8 +10,10 @@
 #include <powsybl/iidm/Network.hpp>
 #include <powsybl/iidm/TwoWindingsTransformer.hpp>
 #include <powsybl/iidm/extensions/iidm/TwoWindingsTransformerPhaseAngleClock.hpp>
+#include <powsybl/iidm/extensions/iidm/TwoWindingsTransformerPhaseAngleClockAdder.hpp>
 #include <powsybl/network/EurostagFactory.hpp>
 
+#include <powsybl/test/AssertionUtils.hpp>
 #include <powsybl/test/ResourceFixture.hpp>
 #include <powsybl/test/converter/RoundTrip.hpp>
 
@@ -25,12 +27,29 @@ namespace iidm {
 
 BOOST_AUTO_TEST_SUITE(TwoWindingsTransformerPhaseAngleClockTestSuite)
 
+BOOST_AUTO_TEST_CASE(integrity) {
+    Network network = powsybl::network::EurostagFactory::createTutorial1Network();
+
+    TwoWindingsTransformer& transformer = network.getTwoWindingsTransformer("NHV2_NLOAD");
+    POWSYBL_ASSERT_THROW(transformer.newExtension<TwoWindingsTransformerPhaseAngleClockAdder>().add(), PowsyblException, "Undefined value for phaseAngleClock");
+    POWSYBL_ASSERT_THROW(transformer.newExtension<TwoWindingsTransformerPhaseAngleClockAdder>().withPhaseAngleClock(12UL).add(), PowsyblException, "Unexpected value for phaseAngleClock: 12");
+    POWSYBL_ASSERT_THROW(TwoWindingsTransformerPhaseAngleClock(transformer, 12UL), PowsyblException, "Unexpected value for phaseAngleClock: 12");
+
+    transformer.newExtension<TwoWindingsTransformerPhaseAngleClockAdder>().withPhaseAngleClock(3UL).add();
+
+    auto& twtpac = transformer.getExtension<TwoWindingsTransformerPhaseAngleClock>();
+    BOOST_CHECK_EQUAL(3, twtpac.getPhaseAngleClock());
+    BOOST_CHECK(stdcxx::areSame(twtpac, twtpac.setPhaseAngleClock(4UL)));
+    BOOST_CHECK_EQUAL(4, twtpac.getPhaseAngleClock());
+    POWSYBL_ASSERT_THROW(twtpac.setPhaseAngleClock(12), PowsyblException, "Unexpected value for phaseAngleClock: 12");
+}
+
 BOOST_FIXTURE_TEST_CASE(TwoWindingsTransformerPhaseAngleClockXmlSerializerTest, test::ResourceFixture) {
     Network network = powsybl::network::EurostagFactory::createTutorial1Network();
     network.setCaseDate(stdcxx::DateTime::parse("2019-05-27T12:17:02.504+02:00"));
 
     TwoWindingsTransformer& transformer = network.getTwoWindingsTransformer("NHV2_NLOAD");
-    transformer.addExtension(Extension::create<TwoWindingsTransformerPhaseAngleClock>(transformer, 3));
+    transformer.newExtension<TwoWindingsTransformerPhaseAngleClockAdder>().withPhaseAngleClock(3).add();
 
     const std::string& networkStr = ResourceFixture::getResource("twoWindingsTransformerPhaseAngleClock.xml");
 
