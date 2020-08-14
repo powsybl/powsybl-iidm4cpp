d772327917e6a2538c848e3465d3ee3fc043c2ab
Sébastien LAIGRE <slaigre@silicom.fr>
2020-08-14 20:23:59 +0200

    Test clang-tidy error in cpp file
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- src/iidm/BusbarSectionAdder.cpp
+++ src/iidm/BusbarSectionAdder.cpp
@@ -8,8 +8,8 @@
 #include <powsybl/iidm/BusbarSectionAdder.hpp>
 
 #include <powsybl/iidm/BusbarSection.hpp>
-#include <powsybl/iidm/Network.hpp>
 #include <powsybl/iidm/Terminal.hpp>
+#include <powsybl/iidm/Network.hpp>
 #include <powsybl/iidm/ValidationUtils.hpp>
 
 #include "NodeBreakerVoltageLevel.hpp"
