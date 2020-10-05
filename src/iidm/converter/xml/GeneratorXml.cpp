ecbe7e14d4200eaf264856fb606c50facdd086c9
Sébastien LAIGRE <slaigre@silicom.fr>
2020-10-05 11:06:17 +0200

    Use network accessor from connectable when it is possible
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- src/iidm/converter/xml/GeneratorXml.cpp
+++ src/iidm/converter/xml/GeneratorXml.cpp
@@ -64,7 +64,7 @@ void GeneratorXml::readSubElements(Generator& generator, NetworkXmlReaderContext
             const std::string& id = context.getAnonymizer().deanonymizeString(context.getReader().getOptionalAttributeValue(ID, ""));
             const std::string& side = context.getReader().getOptionalAttributeValue(SIDE, "");
             context.addEndTask([&generator, id, side]() {
-                generator.setRegulatingTerminal(stdcxx::ref(TerminalRefXml::readTerminalRef(generator.getTerminal().getVoltageLevel().getSubstation().getNetwork(), id, side)));
+                generator.setRegulatingTerminal(stdcxx::ref(TerminalRefXml::readTerminalRef(generator.getNetwork(), id, side)));
             });
         } else if (context.getReader().getLocalName() == REACTIVE_CAPABILITY_CURVE ||
                    context.getReader().getLocalName() == MIN_MAX_REACTIVE_LIMITS) {
