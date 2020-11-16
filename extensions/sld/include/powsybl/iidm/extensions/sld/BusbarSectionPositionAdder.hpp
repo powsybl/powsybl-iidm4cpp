13bb37dbcb10cfef51281c2513b357a784b0d497
Sébastien Laigre <slaigre@silicom.fr>
2020-11-16 19:39:44 +0100

    [IIDM v1.3] Add BusbarSectionPosition (#191) (#214)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

---
+++ extensions/sld/include/powsybl/iidm/extensions/sld/BusbarSectionPositionAdder.hpp
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef POWSYBL_IIDM_EXTENSIONS_SLD_BUSBARSECTIONPOSITIONADDER_HPP
+#define POWSYBL_IIDM_EXTENSIONS_SLD_BUSBARSECTIONPOSITIONADDER_HPP
+
+#include <powsybl/iidm/ExtensionAdder.hpp>
+#include <powsybl/stdcxx/optional.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace extensions {
+
+namespace sld {
+
+class BusbarSectionPositionAdder : public ExtensionAdder {
+public:
+    /**
+     * Constructor
+     */
+    explicit BusbarSectionPositionAdder(Extendable& extendable);
+
+    /**
+     * Destructor
+     */
+    ~BusbarSectionPositionAdder() noexcept override = default;
+
+    /**
+     * Copy constructor
+     */
+    BusbarSectionPositionAdder(const BusbarSectionPositionAdder&) = default;
+
+    /**
+     * Move constructor
+     */
+    BusbarSectionPositionAdder(BusbarSectionPositionAdder&&) = default;
+
+    /**
+     * Copy assignment operator
+     */
+    BusbarSectionPositionAdder& operator=(const BusbarSectionPositionAdder&) = delete;
+
+    /**
+     * Move assignment operator
+     */
+    BusbarSectionPositionAdder& operator=(BusbarSectionPositionAdder&&) = delete;
+
+    /**
+     * Set busbar index
+     *
+     * @param busbarIndex the busbar index
+     *
+     * @return this BusbarSectionPositionAdder object
+     */
+    BusbarSectionPositionAdder& withBusbarIndex(unsigned long busbarIndex);
+
+    /**
+     * Set section index
+     *
+     * @param sectionIndex the section index
+     *
+     * @return this BusbarSectionPositionAdder object
+     */
+    BusbarSectionPositionAdder& withSectionIndex(unsigned long sectionIndex);
+
+protected:
+    /**
+     * Creates the BusbarSectionPosition extension.
+     *
+     * @param extendable the extendable
+     *
+     * @return the extension
+     */
+    std::unique_ptr<Extension> createExtension(Extendable& extendable) override;
+
+private:
+    stdcxx::optional<unsigned long> m_busbarIndex;
+
+    stdcxx::optional<unsigned long> m_sectionIndex;
+};
+
+}  // namespace sld
+
+}  // namespace extensions
+
+}  // namespace iidm
+
+}  // namespace powsybl
+
+#endif  // POWSYBL_IIDM_EXTENSIONS_SLD_BUSBARSECTIONPOSITIONADDER_HPP
