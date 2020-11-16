6213af0a5284c651a194d0668e162f9e537edede
Sébastien Laigre <slaigre@silicom.fr>
2020-11-16 18:16:00 +0100

    [IIDM v1.3] Add SlackTerminal extension (#200)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

---
+++ include/powsybl/iidm/util/TerminalFinder.hpp
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef POWSYBL_IIDM_UTIL_TERMINALFINDER_HPP
+#define POWSYBL_IIDM_UTIL_TERMINALFINDER_HPP
+
+#include <functional>
+#include <vector>
+
+#include <powsybl/stdcxx/range.hpp>
+#include <powsybl/stdcxx/reference_wrapper.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+class Terminal;
+
+namespace util {
+
+class TerminalFinder {
+public:
+    using Comparator = std::function<bool(const Terminal& t1, const Terminal& t2)>;
+
+    using Predicate = std::function<bool(const Terminal&)>;
+
+public:
+    static TerminalFinder getDefault();
+
+public:
+    explicit TerminalFinder(const std::vector<Predicate>& predicates);
+
+    explicit TerminalFinder(const Comparator& comparator);
+
+    TerminalFinder(const TerminalFinder&) = delete;
+
+    TerminalFinder(TerminalFinder&&) noexcept = default;
+
+    TerminalFinder& operator=(const TerminalFinder&) = delete;
+
+    TerminalFinder& operator=(TerminalFinder&&) noexcept = default;
+
+    stdcxx::CReference<Terminal> find(const stdcxx::const_range<Terminal>& terminals) const;
+
+    stdcxx::Reference<Terminal> find(const stdcxx::range<Terminal>& terminals) const;
+
+private:
+    static Comparator getComparator(const std::vector<Predicate>& predicates);
+
+    static const std::vector<Predicate>& getDefaultRules();
+
+private:
+    Comparator m_comparator;
+};
+
+}  // namespace util
+
+}  // namespace iidm
+
+}  // namespace powsybl
+
+#endif  // POWSYBL_IIDM_UTIL_TERMINALFINDER_HPP
