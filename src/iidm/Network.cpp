3a684bd0b34b314c831233283e1bc9628d54fe75
Mathieu BAGUE <mathieu.bague@rte-france.com>
2020-11-23 12:28:47 +0100

    Refactor variants (#218)
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

--- src/iidm/Network.cpp
+++ src/iidm/Network.cpp
@@ -70,10 +70,7 @@ Network::Network(const std::string& id, const std::string& sourceFormat) :
     Container(id, id, false, Container::Type::NETWORK),
     m_sourceFormat(checkNotEmpty(*this, sourceFormat, "Source format is empty")),
     m_variantManager(*this),
-    m_variants(
-            [this]() -> const VariantManager& { return getVariantManager(); },
-            []() { return stdcxx::make_unique<network::VariantImpl>(); }
-    ),
+    m_variants(*this, [this]() { return stdcxx::make_unique<network::VariantImpl>(*this); }),
     m_busBreakerView(*this),
     m_busView(*this) {
     checkAndAdd(std::unique_ptr<Network>(this));
@@ -87,10 +84,7 @@ Network::Network(Network&& network) noexcept :
     m_sourceFormat(std::move(network.m_sourceFormat)),
     m_networkIndex(*this, std::move(network.m_networkIndex)),
     m_variantManager(*this, std::move(network.m_variantManager)),
-    m_variants(
-            [this]() -> const VariantManager& { return getVariantManager(); },
-            []() { return stdcxx::make_unique<network::VariantImpl>(); }  // FIXME(mathbagu): how to move the variants?
-    ),
+    m_variants(*this, std::move(network.m_variants)),
     m_busBreakerView(*this),
     m_busView(*this) {
 }
