70488c46b3e8a847a49cb59ece263b3f9fddece2
Sébastien LAIGRE <slaigre@silicom.fr>
2020-12-20 23:31:29 +0100

    Fix XML read/write of shunts
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- src/iidm/converter/xml/ShuntCompensatorXml.cpp
+++ src/iidm/converter/xml/ShuntCompensatorXml.cpp
@@ -8,6 +8,7 @@
 #include "ShuntCompensatorXml.hpp"
 
 #include <powsybl/iidm/converter/Constants.hpp>
+#include <powsybl/iidm/converter/xml/TerminalRefXml.hpp>
 
 namespace powsybl {
 
@@ -34,6 +35,14 @@ ShuntCompensator& ShuntCompensatorXml::readRootElementAttributes(ShuntCompensato
     auto bPerSection = context.getReader().getAttributeValue<double>(B_PER_SECTION);
     auto maximumSectionCount = context.getReader().getAttributeValue<unsigned long>(MAXIMUM_SECTION_COUNT);
     auto currentSectionCount = context.getReader().getAttributeValue<unsigned long>(CURRENT_SECTION_COUNT);
+    IidmXmlUtil::runFromMinimumVersion(IidmXmlVersion::V1_2(), context.getVersion(), [&context, &adder]() {
+        bool voltageRegulatorOn = context.getReader().getOptionalAttributeValue(VOLTAGE_REGULATOR_ON, false);
+        double targetV = context.getReader().getOptionalAttributeValue(TARGET_V, stdcxx::nan());
+        double targetDeadband = context.getReader().getOptionalAttributeValue(TARGET_DEADBAND, stdcxx::nan());
+        adder.setVoltageRegulatorOn(voltageRegulatorOn)
+            .setTargetV(targetV)
+            .setTargetDeadband(targetDeadband);
+    });
     adder.setbPerSection(bPerSection)
         .setMaximumSectionCount(maximumSectionCount)
         .setCurrentSectionCount(currentSectionCount);
@@ -45,7 +54,16 @@ ShuntCompensator& ShuntCompensatorXml::readRootElementAttributes(ShuntCompensato
 
 void ShuntCompensatorXml::readSubElements(ShuntCompensator& shuntCompensator, NetworkXmlReaderContext& context) const {
     context.getReader().readUntilEndElement(SHUNT, [this, &shuntCompensator, &context]() {
-        AbstractIdentifiableXml::readSubElements(shuntCompensator, context);
+        if (context.getReader().getLocalName() == REGULATING_TERMINAL) {
+            IidmXmlUtil::assertMinimumVersion(SHUNT, REGULATING_TERMINAL, ErrorMessage::NOT_SUPPORTED, IidmXmlVersion::V1_2(), context.getVersion());
+            const auto& id = context.getAnonymizer().deanonymizeString(context.getReader().getAttributeValue(ID));
+            const auto& side = context.getReader().getOptionalAttributeValue(SIDE, "");
+            context.addEndTask([id, side, &shuntCompensator]() {
+                shuntCompensator.setRegulatingTerminal(stdcxx::ref<Terminal>(TerminalRefXml::readTerminalRef(shuntCompensator.getNetwork(), id, side)));
+            });
+        } else {
+            AbstractIdentifiableXml::readSubElements(shuntCompensator, context);
+        }
     });
 }
 
@@ -53,8 +71,20 @@ void ShuntCompensatorXml::writeRootElementAttributes(const ShuntCompensator& shu
     context.getWriter().writeAttribute(B_PER_SECTION, shuntCompensator.getbPerSection());
     context.getWriter().writeAttribute(MAXIMUM_SECTION_COUNT, shuntCompensator.getMaximumSectionCount());
     context.getWriter().writeAttribute(CURRENT_SECTION_COUNT, shuntCompensator.getCurrentSectionCount());
+    IidmXmlUtil::writeBooleanAttributeFromMinimumVersion(SHUNT, VOLTAGE_REGULATOR_ON, shuntCompensator.isVoltageRegulatorOn(), false, ErrorMessage::NOT_DEFAULT_NOT_SUPPORTED, IidmXmlVersion::V1_2(), context);
+    IidmXmlUtil::writeDoubleAttributeFromMinimumVersion(SHUNT, TARGET_V, shuntCompensator.getTargetV(), ErrorMessage::NOT_DEFAULT_NOT_SUPPORTED, IidmXmlVersion::V1_2(), context);
+    IidmXmlUtil::writeDoubleAttributeFromMinimumVersion(SHUNT, TARGET_DEADBAND, shuntCompensator.getTargetDeadband(), ErrorMessage::NOT_DEFAULT_NOT_SUPPORTED, IidmXmlVersion::V1_2(), context);
     writeNodeOrBus(shuntCompensator.getTerminal(), context);
     writePQ(shuntCompensator.getTerminal(), context.getWriter());
+}
+
+void ShuntCompensatorXml::writeSubElements(const ShuntCompensator& shuntCompensator, const VoltageLevel& /*voltageLevel*/, NetworkXmlWriterContext& context) const {
+    if (!stdcxx::areSame(shuntCompensator, shuntCompensator.getRegulatingTerminal().getConnectable().get())) {
+        IidmXmlUtil::assertMinimumVersion(SHUNT, REGULATING_TERMINAL, ErrorMessage::NOT_DEFAULT_NOT_SUPPORTED, IidmXmlVersion::V1_2(), context.getVersion());
+        IidmXmlUtil::runFromMinimumVersion(IidmXmlVersion::V1_2(), context.getVersion(), [&context, &shuntCompensator]() {
+            TerminalRefXml::writeTerminalRef(shuntCompensator.getRegulatingTerminal(), context, REGULATING_TERMINAL);
+        });
+    }
 }
 
 }  // namespace xml
---
+++ test/iidm/converter/xml/ShuntCompensatorRoundTripTest.cpp
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2020, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include <powsybl/test/ResourceFixture.hpp>
+#include <powsybl/test/converter/RoundTrip.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace converter {
+
+namespace xml {
+
+BOOST_AUTO_TEST_SUITE(ShuntCompensatorRoundTrip)
+
+BOOST_FIXTURE_TEST_CASE(ShuntCompensatorRoundTripTest, test::ResourceFixture) {
+    test::converter::RoundTrip::roundTripVersionedXmlTest("shuntRoundTripRef.xml", IidmXmlVersion::CURRENT_IIDM_XML_VERSION());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace xml
+
+}  // namespace converter
+
+}  // namespace iidm
+
+}  // namespace powsybl
