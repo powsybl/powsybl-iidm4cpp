64ceef2627924066aa634b17178f34fc6dbedf51
Sébastien LAIGRE <slaigre@silicom.fr>
2021-02-01 08:46:58 +0100

    Use LoadFooExt and LoadBarExt to simplify test
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- test/iidm/ExtensionTest.cpp
+++ test/iidm/ExtensionTest.cpp
@@ -11,6 +11,8 @@
 #include <powsybl/iidm/Load.hpp>
 #include <powsybl/iidm/Network.hpp>
 #include <powsybl/network/EurostagFactory.hpp>
+#include <powsybl/network/LoadBarExt.hpp>
+#include <powsybl/network/LoadFooExt.hpp>
 
 namespace powsybl {
 
@@ -18,104 +20,25 @@ namespace iidm {
 
 BOOST_AUTO_TEST_SUITE(ExtensionTestSuite)
 
-class LoadExtension : public Extension {
-public:  // Extension
-    const std::string& getName() const override;
-
-    const std::type_index& getType() const override;
-
-public:
-    explicit LoadExtension(Load& load, double value);
-
-    ~LoadExtension() override = default;
-
-    double getValue() const;
-
-private:  // Extension
-    void assertExtendable(const stdcxx::Reference<Extendable>& extendable) const override;
-
-private:
-    double m_value;
-};
-
-LoadExtension::LoadExtension(Load& load, double value) :
-    Extension(load),
-    m_value(value) {
-}
-
-void LoadExtension::assertExtendable(const stdcxx::Reference<Extendable>& extendable) const {
-    if (extendable && !stdcxx::isInstanceOf<Load>(extendable.get())) {
-        throw AssertionError(stdcxx::format("Unexpected extendable type: %1% (%2% expected)", stdcxx::demangle(extendable.get()), stdcxx::demangle<Load>()));
-    }
-}
-
-const std::string& LoadExtension::getName() const {
-    static std::string s_name = "loadExtension";
-    return s_name;
-}
-
-const std::type_index& LoadExtension::getType() const {
-    static std::type_index s_type = typeid(LoadExtension);
-    return s_type;
-}
-
-double LoadExtension::getValue() const {
-    return m_value;
-}
-
-class BatteryExtension : public Extension {
-public:  // Extension
-    const std::string& getName() const override;
-
-    const std::type_index& getType() const override;
-
-public:
-    explicit BatteryExtension(Battery& battery);
-
-    ~BatteryExtension() override = default;
-
-private:  // Extension
-    void assertExtendable(const stdcxx::Reference<Extendable>& extendable) const override;
-};
-
-BatteryExtension::BatteryExtension(Battery& battery) :
-    Extension(battery) {
-}
-
-const std::string& BatteryExtension::getName() const {
-    static std::string s_name = "batteryExtension";
-    return s_name;
-}
-
-const std::type_index& BatteryExtension::getType() const {
-    static std::type_index s_type = typeid(BatteryExtension);
-    return s_type;
-}
-
-void BatteryExtension::assertExtendable(const stdcxx::Reference<Extendable>& extendable) const {
-    if (extendable && !stdcxx::isInstanceOf<Battery>(extendable.get())) {
-        throw AssertionError(stdcxx::format("Unexpected extendable type: %1% (%2% expected)", stdcxx::demangle(extendable.get()), stdcxx::demangle<Battery>()));
-    }
-}
-
 BOOST_AUTO_TEST_CASE(getExtensionByName) {
     Network network = powsybl::network::EurostagFactory::createTutorial1Network();
     Load& load = network.getLoad("LOAD");
-    load.addExtension(stdcxx::make_unique<LoadExtension>(load, 1.1));
+    load.addExtension(stdcxx::make_unique<powsybl::network::LoadFooExt>(load));
     const Load& cLoad = load;
 
-    BOOST_CHECK(!cLoad.getExtensionByName<LoadExtension>("test"));
-    BOOST_CHECK(!load.getExtensionByName<LoadExtension>("test"));
+    BOOST_CHECK(!cLoad.getExtensionByName<powsybl::network::LoadFooExt>("test"));
+    BOOST_CHECK(!load.getExtensionByName<powsybl::network::LoadFooExt>("test"));
 
-    BOOST_CHECK(!cLoad.getExtensionByName<BatteryExtension>("test"));
-    BOOST_CHECK(!load.getExtensionByName<BatteryExtension>("test"));
+    BOOST_CHECK(cLoad.getExtensionByName<Extension>("loadFoo"));
+    BOOST_CHECK(load.getExtensionByName<Extension>("loadFoo"));
 
-    auto cExt = cLoad.getExtensionByName<LoadExtension>("loadExtension");
-    auto ext = load.getExtensionByName<LoadExtension>("loadExtension");
+    BOOST_CHECK(!cLoad.getExtensionByName<powsybl::network::LoadBarExt>("loadFoo"));
+    BOOST_CHECK(!load.getExtensionByName<powsybl::network::LoadBarExt>("loadFoo"));
+
+    auto cExt = cLoad.getExtensionByName<powsybl::network::LoadFooExt>("loadFoo");
+    auto ext = load.getExtensionByName<powsybl::network::LoadFooExt>("loadFoo");
     BOOST_CHECK(cExt);
     BOOST_CHECK(ext);
-    BOOST_CHECK_CLOSE(1.1, cExt.get().getValue(), std::numeric_limits<double>::epsilon());
-    BOOST_CHECK_CLOSE(1.1, ext.get().getValue(), std::numeric_limits<double>::epsilon());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
