56f2bef7b78bdea67f12d200aeb891ba300a0327
Mathieu BAGUE <mathieu.bague@rte-france.com>
2021-03-25 10:24:01 +0100

    Make registerExtension public (#303)
    * Make registerExtension public
    * Add an example to show how to use an XIIDM extension
    
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

---
+++ examples/example3/example3.cpp
@@ -0,0 +1,61 @@
+/**
+ * @internal
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * @endinternal
+ */
+
+#include <iostream>
+
+#include <libxml/parser.h>
+
+#include <powsybl/PowsyblException.hpp>
+#include <powsybl/iidm/ExtensionProviders.hpp>
+#include <powsybl/iidm/Generator.hpp>
+#include <powsybl/iidm/Network.hpp>
+#include <powsybl/iidm/converter/xml/ExtensionXmlSerializer.hpp>
+#include <powsybl/iidm/extensions/iidm/ActivePowerControl.hpp>
+#include <powsybl/iidm/extensions/iidm/ActivePowerControlAdder.hpp>
+#include <powsybl/iidm/extensions/iidm/ActivePowerControlXmlSerializer.hpp>
+#include <powsybl/network/EurostagFactory.hpp>
+
+namespace xml = powsybl::iidm::converter::xml;
+namespace ext = powsybl::iidm::extensions::iidm;
+
+/**
+ * @example example3.cpp
+ * This is an example of how to work with IIDM extensions.
+ */
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Usage: " << argv[0] << " <output-file>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    try {
+        xmlInitParser();
+
+        powsybl::iidm::Network network = powsybl::network::EurostagFactory::createTutorial1Network();
+
+        network.getGenerator("GEN").newExtension<ext::ActivePowerControlAdder>()
+            .withDroop(4.0)
+            .withParticipate(true)
+            .add();
+
+        // Register the serializer for the ActivePowerControl extension
+        auto& extProvider = powsybl::iidm::ExtensionProviders<xml::ExtensionXmlSerializer>::getInstance();
+        extProvider.registerExtension(stdcxx::make_unique<ext::ActivePowerControlXmlSerializer>());
+
+        powsybl::iidm::Network::writeXml(argv[1], network);
+
+        xmlCleanupParser();
+
+    } catch (const powsybl::PowsyblException& e) {
+        std::cout << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
