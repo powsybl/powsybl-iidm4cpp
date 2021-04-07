3b81bdc595fe88445a96c4672463ff2d88b4126b
Gautier Bureau <gautier.bureau@gmail.com>
2021-04-07 15:02:30 +0200

    Corrections after review of handle of static/shared compilation.
    Signed-off-by: Gautier Bureau <gautier.bureau@gmail.com>

--- extensions/iidm/test/iidm.cpp
+++ extensions/iidm/test/iidm.cpp
@@ -8,14 +8,48 @@
 #define BOOST_TEST_MODULE iidm
 #include <boost/test/unit_test.hpp>
 
+#include <powsybl/iidm/ExtensionProviders.hpp>
+#include <powsybl/iidm/converter/xml/ExtensionXmlSerializer.hpp>
+#include <powsybl/iidm/extensions/iidm/ActivePowerControlXmlSerializer.hpp>
+#include <powsybl/iidm/extensions/iidm/CoordinatedReactiveControlXmlSerializer.hpp>
+#include <powsybl/iidm/extensions/iidm/ThreeWindingsTransformerPhaseAngleClockXmlSerializer.hpp>
+#include <powsybl/iidm/extensions/iidm/TwoWindingsTransformerPhaseAngleClockXmlSerializer.hpp>
+#include <powsybl/iidm/extensions/iidm/VoltagePerReactivePowerControlXmlSerializer.hpp>
 #include <powsybl/test/ExtensionFixture.hpp>
 #include <powsybl/test/XmlFixture.hpp>
+
+namespace xml = powsybl::iidm::converter::xml;
+namespace ext = powsybl::iidm::extensions::iidm;
 
 namespace powsybl {
 
 namespace test {
 
-BOOST_TEST_GLOBAL_FIXTURE(ExtensionFixture);
+class ExtensionFixtureIidm : public ResourceFixture {
+ public:
+  ExtensionFixtureIidm();
+
+  ExtensionFixtureIidm(const ExtensionFixtureIidm&) = default;
+
+  ExtensionFixtureIidm(ExtensionFixtureIidm&&) = default;
+
+  ~ExtensionFixtureIidm() = default;
+
+  ExtensionFixtureIidm& operator=(const ExtensionFixtureIidm&) = default;
+
+  ExtensionFixtureIidm& operator=(ExtensionFixtureIidm&&) = default;
+};
+
+ExtensionFixtureIidm::ExtensionFixtureIidm() {
+  auto& extProvider = powsybl::iidm::ExtensionProviders<xml::ExtensionXmlSerializer>::getInstance();
+  extProvider.registerExtension(stdcxx::make_unique<ext::ActivePowerControlXmlSerializer>());
+  extProvider.registerExtension(stdcxx::make_unique<ext::CoordinatedReactiveControlXmlSerializer>());
+  extProvider.registerExtension(stdcxx::make_unique<ext::ThreeWindingsTransformerPhaseAngleClockXmlSerializer>());
+  extProvider.registerExtension(stdcxx::make_unique<ext::TwoWindingsTransformerPhaseAngleClockXmlSerializer>());
+  extProvider.registerExtension(stdcxx::make_unique<ext::VoltagePerReactivePowerControlXmlSerializer>());
+}
+
+BOOST_TEST_GLOBAL_FIXTURE(ExtensionFixtureIidm);
 BOOST_TEST_GLOBAL_FIXTURE(XmlFixture);
 
 }  // namespace test
