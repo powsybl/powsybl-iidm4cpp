589c35e310e0b6ff88516174fe9f07d527da9a72
Sébastien Laigre <slaigre@silicom.fr>
2021-07-05 09:31:31 +0200

    [IIDM v1.5.1] Add new iidm hvdc extensions (#327) (#340)
    * Add HvdcAngleDroopActivePowerControl extension
    
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

---
+++ extensions/iidm/include/powsybl/iidm/extensions/iidm/HvdcOperatorActivePowerRange.hpp
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef POWSYBL_IIDM_EXTENSIONS_IIDM_HVDCOPERATORACTIVEPOWERRANGE_HPP
+#define POWSYBL_IIDM_EXTENSIONS_IIDM_HVDCOPERATORACTIVEPOWERRANGE_HPP
+
+#include <powsybl/iidm/Extension.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+class HvdcConverterStation;
+class HvdcLine;
+
+namespace extensions {
+
+namespace iidm {
+
+class HvdcOperatorActivePowerRange : public Extension {
+public:  // Extension
+    const std::string& getName() const override;
+
+    const std::type_index& getType() const override;
+
+public:
+    HvdcOperatorActivePowerRange(HvdcLine& hvdcLine, double oprFromCS1toCS2, double oprFromCS2toCS1);
+
+    double getOprFromCS1toCS2() const;
+
+    double getOprFromCS2toCS1() const;
+
+    HvdcOperatorActivePowerRange& setOprFromCS1toCS2(double oprFromCS1toCS2);
+
+    HvdcOperatorActivePowerRange& setOprFromCS2toCS1(double oprFromCS2toCS1);
+
+private:  // Extension
+    void assertExtendable(const stdcxx::Reference<Extendable>& extendable) const override;
+
+private:
+    static double checkOPR(double opr, const HvdcConverterStation& from, const HvdcConverterStation& to);
+
+private:
+    /**
+     * Operator active power range from the converter station 1 to the converter station 2 in MW.
+     */
+    double m_oprFromCS1toCS2;
+
+    /**
+     * Operator active power range from the converter station 2 to the converter station 1 in MW.
+     */
+    double m_oprFromCS2toCS1;
+};
+
+}  // namespace iidm
+
+}  // namespace extensions
+
+}  // namespace iidm
+
+}  // namespace powsybl
+
+#endif  // POWSYBL_IIDM_EXTENSIONS_IIDM_HVDCOPERATORACTIVEPOWERRANGE_HPP
---
+++ extensions/iidm/src/HvdcAngleDroopActivePowerControlXmlSerializer.cpp
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <powsybl/iidm/extensions/iidm/HvdcAngleDroopActivePowerControlXmlSerializer.hpp>
+
+#include <powsybl/iidm/HvdcLine.hpp>
+#include <powsybl/iidm/converter/xml/NetworkXmlReaderContext.hpp>
+#include <powsybl/iidm/converter/xml/NetworkXmlWriterContext.hpp>
+#include <powsybl/iidm/extensions/iidm/HvdcAngleDroopActivePowerControl.hpp>
+#include <powsybl/stdcxx/math.hpp>
+#include <powsybl/xml/XmlStreamReader.hpp>
+#include <powsybl/xml/XmlStreamWriter.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace extensions {
+
+namespace iidm {
+
+HvdcAngleDroopActivePowerControlXmlSerializer::HvdcAngleDroopActivePowerControlXmlSerializer() :
+    AbstractExtensionXmlSerializer("hvdcAngleDroopActivePowerControl", "network", "hapc", "http://www.itesla_project.eu/schema/iidm/ext/hvdc_angle_droop_active_power_control/1_0") {
+}
+
+Extension& HvdcAngleDroopActivePowerControlXmlSerializer::read(Extendable& extendable, converter::xml::NetworkXmlReaderContext& context) const {
+    if (!stdcxx::isInstanceOf<HvdcLine>(extendable)) {
+        throw AssertionError(stdcxx::format("Unexpected extendable type: %1% (%2% expected)", stdcxx::demangle(extendable), stdcxx::demangle<HvdcLine>()));
+    }
+    auto& hvdcLine = dynamic_cast<HvdcLine&>(extendable);
+
+    const double& p0 = context.getReader().getAttributeValue<double>("p0");
+    const double& droop = context.getReader().getAttributeValue<double>("droop");
+    const auto& enabled = context.getReader().getAttributeValue<bool>("enabled");
+    extendable.addExtension(stdcxx::make_unique<HvdcAngleDroopActivePowerControl>(hvdcLine, p0, droop, enabled));
+    return extendable.getExtension<HvdcAngleDroopActivePowerControl>();
+}
+
+void HvdcAngleDroopActivePowerControlXmlSerializer::write(const Extension& extension, converter::xml::NetworkXmlWriterContext& context) const {
+    const auto& hadap = safeCast<HvdcAngleDroopActivePowerControl>(extension);
+    context.getWriter().writeAttribute("p0", hadap.getP0());
+    context.getWriter().writeAttribute("droop", hadap.getDroop());
+    context.getWriter().writeAttribute("enabled", hadap.isEnabled());
+}
+
+}  // namespace iidm
+
+}  // namespace extensions
+
+}  // namespace iidm
+
+}  // namespace powsybl
