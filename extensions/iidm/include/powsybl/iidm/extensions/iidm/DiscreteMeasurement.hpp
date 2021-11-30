1ac18f7ccf51e51fc6c08cf2520fac1dbf7c24bc
Sébastien Laigre <slaigre@silicom.fr>
2021-11-30 12:05:11 +0100

    Add Measurements and DiscreteMeasurements extensions (#361) (#376)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>
    Signed-off-by: Mathieu BAGUE <mathieu.bague@rte-france.com>

---
+++ extensions/iidm/include/powsybl/iidm/extensions/iidm/DiscreteMeasurement.hpp
@@ -0,0 +1,203 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef POWSYBL_IIDM_EXTENSIONS_IIDM_DISCRETEMEASUREMENT_HPP
+#define POWSYBL_IIDM_EXTENSIONS_IIDM_DISCRETEMEASUREMENT_HPP
+
+#include <iosfwd>
+#include <map>
+#include <string>
+
+#include <boost/any.hpp>
+
+#include <powsybl/stdcxx/optional.hpp>
+#include <powsybl/stdcxx/range.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace extensions {
+
+namespace iidm {
+
+class DiscreteMeasurements;
+
+class DiscreteMeasurement {
+public:
+    /**
+     * Specify what is measured.
+     */
+    enum class Type : unsigned char {
+        TAP_POSITION,
+        SWITCH_POSITION,
+        SHUNT_COMPENSATOR_SECTION,
+        OTHER
+    };
+
+    /**
+     * If it is the modelization of a tap position (or any discrete measurement on a tap changer),
+     * can explicit which tap changer of the transformer it is applied on.
+     */
+    enum class TapChanger : unsigned char {
+        RATIO_TAP_CHANGER,
+        PHASE_TAP_CHANGER,
+        RATIO_TAP_CHANGER_1,
+        PHASE_TAP_CHANGER_1,
+        RATIO_TAP_CHANGER_2,
+        PHASE_TAP_CHANGER_2,
+        RATIO_TAP_CHANGER_3,
+        PHASE_TAP_CHANGER_3
+    };
+
+    /**
+     * Explicit what type of discrete value is used for this measurement (boolean, int or string).
+     */
+    enum class ValueType : unsigned char {
+        BOOLEAN,
+        INT,
+        STRING
+    };
+
+public:
+    ~DiscreteMeasurement() = default;
+
+    /**
+     * Get ID of the measurement if it exists. It is optional (can be empty).
+     */
+    const std::string& getId() const;
+
+    /**
+     * Return property value associated to the given property name if it exists, else return an empty string.
+     */
+    std::string getProperty(const std::string& name) const;
+
+    /**
+     * Get all the property names with values applied to this measurement.
+     */
+    stdcxx::const_range<std::string> getPropertyNames() const;
+
+    /**
+     * Get the tap changer the measurement is applied on if necessary (see {@link TapChanger}).
+     * Must be an invalid reference if the measurement is not applied to a tap changer. Throw an exception if the extended equipment is not a transformer.
+     * Must be a valid reference if the measurement is not applied to a tap changer, else throw an exception.
+     */
+    stdcxx::optional<TapChanger> getTapChanger() const;
+
+    /**
+     * Get the type of the measurement (specify what it is measured, see {@link Type}).
+     */
+    const Type& getType() const;
+
+    /**
+     * Get value as boolean if the type of measured discrete value is BOOLEAN (see {@link ValueType}).
+     * Else throw an exception.
+     */
+    bool getValueAsBoolean() const;
+
+    /**
+     * Get value as int if the type of measured discrete value is INT (see {@link ValueType}).
+     * Else throw an exception.
+     */
+    int getValueAsInt() const;
+
+    /**
+     * Get value as String if the type of measured discrete value is STRING (see {@link ValueType}).
+     * Else throw an exception.
+     */
+    std::string getValueAsString() const;
+
+    /**
+     * Get the type of measured discrete value (int, boolean or string, see {@link ValueType}).
+     */
+    const ValueType& getValueType() const;
+
+    /**
+     * Get validity status of the measurement.
+     * If it is true (i.e. the measurement is valid), the discrete measured value can not be null.
+     */
+    bool isValid() const;
+
+    /**
+     * Put a given property value associated with a given property name.
+     */
+    DiscreteMeasurement& putProperty(const std::string& name, const std::string& property);
+
+    /**
+     * Remove the measurement from the equipment.
+     */
+    void remove();
+
+    /**
+     * Remove the property value associated with the given property name if it exists. Else, do nothing.
+     */
+    DiscreteMeasurement& removeProperty(const std::string& name);
+
+    /**
+     * Set validity status of the measurement.
+     * If it is true (i.e. the measurement is valid), the discrete measured value can not be null.
+     */
+    DiscreteMeasurement& setValid(bool valid);
+
+    /**
+     * Set the discrete measured value and set the value type as STRING (see {@link ValueType}).
+     * Can not be empty if the measurement is valid.
+     */
+    DiscreteMeasurement& setValue(const std::string& value);
+
+    DiscreteMeasurement& setValue(const char* value);
+
+    /**
+     * Set the discrete measured value and set the value type as BOOLEAN (see {@link ValueType}).
+     */
+    DiscreteMeasurement& setValue(bool value);
+
+    /**
+     * Set the discrete measured value and set the value type as INT (see {@link ValueType}).
+     */
+    DiscreteMeasurement& setValue(int value);
+
+private:
+    DiscreteMeasurement(DiscreteMeasurements& discreteMeasurements, const std::string& id, const Type& type,
+                        const stdcxx::optional<TapChanger>& tapChanger, const std::map<std::string, std::string>& properties,
+                        const ValueType& valueType, const boost::any& value, bool valid);
+
+    friend class DiscreteMeasurementAdder;
+
+private:
+    DiscreteMeasurements& m_discreteMeasurements;
+
+    std::string m_id;
+
+    Type m_type;
+
+    stdcxx::optional<TapChanger> m_tapChanger;
+
+    std::map<std::string, std::string> m_properties;
+
+    ValueType m_valueType;
+
+    boost::any m_value;
+
+    bool m_valid;
+};
+
+std::ostream& operator<<(std::ostream& stream, const DiscreteMeasurement::Type& type);
+
+std::ostream& operator<<(std::ostream& stream, const DiscreteMeasurement::TapChanger& tapChanger);
+
+std::ostream& operator<<(std::ostream& stream, const DiscreteMeasurement::ValueType& valueType);
+
+}  // namespace iidm
+
+}  // namespace extensions
+
+}  // namespace iidm
+
+}  // namespace powsybl
+
+#endif  // POWSYBL_IIDM_EXTENSIONS_IIDM_DISCRETEMEASUREMENT_HPP
---
+++ extensions/iidm/src/Measurement.cpp
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <powsybl/iidm/extensions/iidm/Measurement.hpp>
+
+#include <boost/range/adaptor/map.hpp>
+
+#include <powsybl/iidm/Enum.hpp>
+#include <powsybl/iidm/extensions/iidm/Measurements.hpp>
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace Enum {
+
+template <>
+const std::initializer_list<std::string>& getNames<extensions::iidm::Measurement::Side>() {
+    static std::initializer_list<std::string> s_sidesNames {
+        "ONE",
+        "TWO",
+        "THREE"
+    };
+    return s_sidesNames;
+}
+
+template <>
+const std::initializer_list<std::string>& getNames<extensions::iidm::Measurement::Type>() {
+    static std::initializer_list<std::string> s_typesNames {
+        "ANGLE",
+        "ACTIVE_POWER",
+        "APPARENT_POWER",
+        "REACTIVE_POWER",
+        "CURRENT",
+        "VOLTAGE",
+        "FREQUENCY",
+        "OTHER"
+    };
+    return s_typesNames;
+}
+
+}  // namespace Enum
+
+namespace extensions {
+
+namespace iidm {
+
+std::ostream& operator<<(std::ostream& stream, const Measurement::Side& value) {
+    stream << Enum::toString(value);
+    return stream;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Measurement::Type& value) {
+    stream << Enum::toString(value);
+    return stream;
+}
+
+Measurement::Measurement(Measurements& measurements, const std::string& id, const Measurement::Type& type, const std::map<std::string, std::string>& properties,
+                         double value, double standardDeviation, bool valid, const stdcxx::optional<Measurement::Side>& side) :
+    m_measurements(measurements),
+    m_id(id),
+    m_type(type),
+    m_properties(properties),
+    m_side(side),
+    m_value(value),
+    m_standardDeviation(standardDeviation),
+    m_valid(valid) {
+}
+
+const std::string& Measurement::getId() const {
+    return m_id;
+}
+
+std::string Measurement::getProperty(const std::string& name) const {
+    const auto& it = m_properties.find(name);
+    return it != m_properties.cend() ? it->second : "";
+}
+
+stdcxx::const_range<std::string> Measurement::getPropertyNames() const {
+    return m_properties | boost::adaptors::map_keys;
+}
+
+const stdcxx::optional<Measurement::Side>& Measurement::getSide() const {
+    return m_side;
+}
+
+double Measurement::getStandardDeviation() const {
+    return m_standardDeviation;
+}
+
+const Measurement::Type& Measurement::getType() const {
+    return m_type;
+}
+
+double Measurement::getValue() const {
+    return m_value;
+}
+
+bool Measurement::isValid() const {
+    return m_valid;
+}
+
+Measurement& Measurement::putProperty(const std::string& name, const std::string& property) {
+    if (name.empty()) {
+        throw PowsyblException("Unexpected empty property name");
+    }
+    m_properties[name] = property;
+    return *this;
+}
+
+void Measurement::remove() {
+    m_measurements.remove(*this);
+}
+
+Measurement& Measurement::removeProperty(const std::string& name) {
+    m_properties.erase(name);
+    return *this;
+}
+
+Measurement& Measurement::setStandardDeviation(double standardDeviation) {
+    m_standardDeviation = standardDeviation;
+    return *this;
+}
+
+Measurement& Measurement::setValid(bool valid) {
+    m_valid = valid;
+    return *this;
+}
+
+Measurement& Measurement::setValue(double value) {
+    m_value = value;
+    return *this;
+}
+
+}  // namespace iidm
+
+}  // namespace extensions
+
+}  // namespace iidm
+
+}  // namespace powsybl
