f9ea91f356caa2c41529dff800eb8a82076b5bc7
Sébastien LAIGRE <slaigre@silicom.fr>
2021-12-17 10:07:00 +0100

    Fix stdcxx::cref(std::unique_ptr<T>) and stdcxx::ref(std::unique_ptr<T>) by handling null pointer case
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- include/powsybl/stdcxx/reference.hpp
+++ include/powsybl/stdcxx/reference.hpp
@@ -104,7 +104,7 @@ CReference<T> cref(T& reference) {
 
 template <typename T>
 CReference<T> cref(const std::unique_ptr<T>& pointer) {
-    return CReference<T>(*pointer);
+    return static_cast<bool>(pointer) ? CReference<T>(*pointer) : CReference<T>();
 }
 
 template <typename T>
@@ -129,7 +129,7 @@ Reference<T> ref(T& reference) {
 
 template <typename T>
 Reference<T> ref(const std::unique_ptr<T>& pointer) {
-    return Reference<T>(*pointer);
+    return static_cast<bool>(pointer) ? Reference<T>(*pointer) : Reference<T>();
 }
 
 template <typename T, typename U, typename = typename std::enable_if<std::is_base_of<T, U>::value && !std::is_same<T, U>::value>::type>
