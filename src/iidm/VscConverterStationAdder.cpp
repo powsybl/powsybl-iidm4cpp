98f67b11ac0af4a8b00cdb6ee2426be351355e5f
Sébastien LAIGRE <slaigre@silicom.fr>
2022-02-01 08:18:27 +0100

    Add regulating terminal for vsc converter station (#392)
    Signed-off-by: Sébastien LAIGRE <slaigre@silicom.fr>

--- src/iidm/VscConverterStationAdder.cpp
+++ src/iidm/VscConverterStationAdder.cpp
@@ -23,10 +23,12 @@ VscConverterStationAdder::VscConverterStationAdder(VoltageLevel& voltageLevel) :
 VscConverterStation& VscConverterStationAdder::add() {
     validate();
 
-    std::unique_ptr<VscConverterStation> ptrVsc = stdcxx::make_unique<VscConverterStation>(getNetwork(), checkAndGetUniqueId(), getName(), isFictitious(), getLossFactor(), *m_voltageRegulatorOn, m_reactivePowerSetpoint, m_voltageSetpoint);
+    auto terminalPtr = checkAndGetTerminal();
+    Terminal& regulatingTerminal = m_regulatingTerminal ? m_regulatingTerminal.get() : *terminalPtr;
+    std::unique_ptr<VscConverterStation> ptrVsc = stdcxx::make_unique<VscConverterStation>(getNetwork(), checkAndGetUniqueId(), getName(), isFictitious(), getLossFactor(), *m_voltageRegulatorOn, m_reactivePowerSetpoint, m_voltageSetpoint, regulatingTerminal);
     auto& vsc = getNetwork().checkAndAdd<VscConverterStation>(std::move(ptrVsc));
 
-    Terminal& terminal = vsc.addTerminal(checkAndGetTerminal());
+    Terminal& terminal = vsc.addTerminal(std::move(terminalPtr));
     getVoltageLevel().attach(terminal, false);
 
     return vsc;
@@ -40,6 +42,11 @@ const std::string& VscConverterStationAdder::getTypeDescription() const {
 
 VscConverterStationAdder& VscConverterStationAdder::setReactivePowerSetpoint(double reactivePowerSetpoint) {
     m_reactivePowerSetpoint = reactivePowerSetpoint;
+    return *this;
+}
+
+VscConverterStationAdder& VscConverterStationAdder::setRegulatingTerminal(const stdcxx::Reference<Terminal>& terminal) {
+    m_regulatingTerminal = terminal;
     return *this;
 }
 
@@ -57,6 +64,7 @@ void VscConverterStationAdder::validate() const {
     HvdcConverterStationAdder::validate();
     checkOptional(*this, m_voltageRegulatorOn, "voltage regulator status is not set");
     checkVoltageControl(*this, *m_voltageRegulatorOn, m_voltageSetpoint, m_reactivePowerSetpoint);
+    checkRegulatingTerminal(*this, m_regulatingTerminal, getNetwork());
 }
 
 }  // namespace iidm
