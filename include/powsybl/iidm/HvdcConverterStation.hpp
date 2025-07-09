d9029bac810602a024a779881e0606ce16924b70
Edouard <edouard.chevreau@dps-fr.com>
2025-07-09 10:07:26 +0200

    Fix CI (#459)
    * Fix CI Windows and linux
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    include chrono
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    Ignore unknown warnings in clangtidy config
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    * Fix clang-tidy warnings
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    Add missing include
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    * Ignore gcovr parse error
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    * Drop MacOS CI for now
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>
    
    ---------
    
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>

--- include/powsybl/iidm/HvdcConverterStation.hpp
+++ include/powsybl/iidm/HvdcConverterStation.hpp
@@ -11,6 +11,8 @@
 #include <powsybl/iidm/Injection.hpp>
 #include <powsybl/stdcxx/reference.hpp>
 
+#include <cstdint>
+
 namespace powsybl {
 
 namespace iidm {
@@ -22,7 +24,7 @@ public:  // Connectable
     void remove() override;
 
 public:
-    enum class HvdcType {
+    enum class HvdcType : std::uint8_t {
         VSC,
         LCC
     };
