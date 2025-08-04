ebc41b6fd630480bd88170b65f6e9e7f91dcdd5f
Edouard Chevreau <edouard.chevreau@dps-fr.com>
2025-08-04 16:39:32 +0200

    Add loading limits test for V1.5 writing issue
    Signed-off-by: Edouard Chevreau <edouard.chevreau@dps-fr.com>

---
+++ test/iidm/converter/xml/LoadingLimitsBugTest.cpp
@@ -0,0 +1,96 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <boost/test/unit_test.hpp>
+
+#include <powsybl/iidm/ActivePowerLimitsAdder.hpp>
+#include <powsybl/iidm/ApparentPowerLimitsAdder.hpp>
+#include <powsybl/iidm/Bus.hpp>
+#include <powsybl/iidm/BusAdder.hpp>
+#include <powsybl/iidm/CurrentLimitsAdder.hpp>
+#include <powsybl/iidm/DanglingLine.hpp>
+#include <powsybl/iidm/DanglingLineAdder.hpp>
+#include <powsybl/iidm/Generator.hpp>
+#include <powsybl/iidm/GeneratorAdder.hpp>
+#include <powsybl/iidm/LoadingLimits.hpp>
+#include <powsybl/iidm/LoadingLimitsAdder.hpp>
+#include <powsybl/iidm/Network.hpp>
+#include <powsybl/iidm/Substation.hpp>
+#include <powsybl/iidm/TwoWindingsTransformer.hpp>
+#include <powsybl/iidm/TwoWindingsTransformerAdder.hpp>
+#include <powsybl/iidm/ThreeWindingsTransformer.hpp>
+#include <powsybl/iidm/TieLine.hpp>
+#include <powsybl/iidm/VoltageLevel.hpp>
+#include <powsybl/iidm/VoltageLevelAdder.hpp>
+#include <powsybl/iidm/converter/xml/IidmXmlVersion.hpp>
+#include <powsybl/network/EurostagFactory.hpp>
+#include <powsybl/network/ThreeWindingsTransformerNetworkFactory.hpp>
+#include <powsybl/test/AssertionUtils.hpp>
+#include <powsybl/test/ResourceFixture.hpp>
+#include <powsybl/test/converter/RoundTrip.hpp>
+
+
+namespace powsybl {
+
+namespace iidm {
+
+namespace converter {
+
+namespace xml {
+
+BOOST_AUTO_TEST_SUITE(LoadingLimitsBugTestRoundTrip)
+
+
+BOOST_FIXTURE_TEST_CASE(LoadingLimitsBugTest, test::ResourceFixture) {
+    
+    Network network = powsybl::network::EurostagFactory::createTutorial1Network();
+    network.setCaseDate(stdcxx::DateTime::parse("2019-05-27T12:17:02.504+02:00"));
+
+    auto& twt = network.getSubstation("P1").newTwoWindingsTransformer()
+                .setId("TWT")
+                .setVoltageLevel1("VLGEN")
+                .setBus1("NGEN")
+                .setConnectableBus1("NGEN")
+                .setRatedU1(24.0)
+                .setVoltageLevel2("VLHV1")
+                .setBus2("NHV1")
+                .setConnectableBus2("NHV1")
+                .setRatedU2(400.0)
+                .setR(1)
+                .setX(1)
+                .setG(0.0)
+                .setB(0.0)
+                .add();
+
+    twt.newApparentPowerLimits1()
+        .setPermanentLimit(100)
+        .add();
+
+    ExportOptions options = ExportOptions().setVersion(IidmXmlVersion::V1_5().toString("."));
+    std::stringstream ss;
+    const std::string& filename = stdcxx::format("%1%.xiidm", network.getId());
+
+    const auto& writer = [&options](const iidm::Network& n, std::ostream& stream) {
+        iidm::Network::writeXml(stdcxx::format("%1%.xiidm", n.getId()), stream, n, options);
+    };
+    
+
+    test::converter::RoundTrip::writeXmlTest(network, writer, test::converter::RoundTrip::getVersionedNetwork("loading-limits-bug.xml", IidmXmlVersion::V1_5()));
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+}  // namespace xml
+
+}  // namespace converter
+
+}  // namespace iidm
+
+}  // namespace powsybl
